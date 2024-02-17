2503e103dba44f984dbd3ddad75ccac1877a7aed
sthaanshuu <anshushrestha9@gmail.com>
2024-02-18 00:02:43 +0545

    Responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,75 +1,102 @@
-import React, { useState } from 'react';
-import { images } from '../constants';
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Link, useNavigate } from "react-router-dom";
+
+import { images } from "../constants";
+
+const navItemsInfo = [
+  { name: "Home", type: "link", url: "/" },
+  { name: "Blog", type: "link", url: "/Blog" },
+  { name: "Mood Tracker", type: "link", url: "/MoodTracker" },
+  { name: "Journal", type: "link", url: "/Journal" },
+];
+
+const NavItem = ({ item }) => {
+  return (
+    <li className="relative group">
+      {item.type === "link" ? (
+        <>
+          <a href={item.url} className="px-4 py-2">
+            {item.name}
+          </a>
+          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+          </span>
+        </>
+      ) : (
+        <>
+          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
+          </a>
+          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
+            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => (
+                <a
+                  href="/"
+                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                >
+                  {page}
+                </a>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
+    </li>
+  );
+};
 
 const Header = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
+  const navigate = useNavigate();
 
-  const handleDropdownChange = () => {
-    // handle dropdown change logic here
+
+  const [navIsVisible, setNavIsVisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsVisible((curState) => {
+      return !curState;
+    });
   };
 
   return (
     <section>
-      <header className="container max-auto px-5 py-4 md:px-10 relative">
-        <div className="flex justify-between items-center">
-          <div>
-          <img src={images.Logo} alt="logo" style={{ width: '150px', height: 'auto' }} />
-          </div>
-          <div className="flex gap-x-3 items-center">
-            <button
-              type="button"
-              className="focus:outline-none md:hidden"
-              onClick={() => setShowDropdown(!showDropdown)}
-            >
-            </button>
-            <ul
-              className={`${
-                showDropdown ? 'block' : 'hidden'
-              } md:flex md:gap-x-5 font-semibold md:items-center`}
-            >
-              <li>
-                <a href="/" className="px-4 transition duration-300 ease-in-out hover:text-blue-500">
-                  Home
-                </a>
-              </li>
-              <li className="relative flex items-center">
-                <button
-                  type="button"
-                  className="transition duration-300 ease-in-out hover:text-blue-500 focus:outline-none flex items-center"
-                  onClick={() => setShowDropdown(!showDropdown)}
-                >
-                  Blog
-                </button>
-                {showDropdown && (
-                  <div className="absolute z-10 top-full left-0 w-40 mt-1 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <div className="py-1">
-                      {/* Dropdown Blog Options */}
-                    </div>
-                  </div>
-                )}
-              </li>
-              <li>
-                <a href="/MoodTracker" className="px-4 transition duration-300 ease-in-out hover:text-blue-500">
-                  Mood Tracker
-                </a>
-              </li>
-              <li>
-                <a href="/Journal" className="px-4 transition duration-300 ease-in-out hover:text-blue-500">
-                  Journal
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <a href="/register" className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white hover:border-blue-500 transition duration-300 ease-in-out">
-              Sign in
-            </a>
-          </div>
+      <header className="container mx-auto px-5 flex justify-between py-4 items-center">
+        <div>
+        <img src=
+          {images.Logo} 
+          alt="logo" 
+          style={{ width: '150px', height: 'auto' }} />
         </div>
-        <div className="absolute bottom-0 left-0 w-full h-0.5 bg-black"></div>
+        <div className="lg:hidden z-50">
+          {navIsVisible ? (
+            <AiOutlineClose
+              className="w-6 h-6"
+              onClick={navVisibilityHandler}
+            />
+          ) : (
+            <AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler} />
+          )}
+        </div>
+        <div
+          className={`${
+            navIsVisible ? "right-0" : "-right-full"
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+            {navItemsInfo.map((item) => (
+              <NavItem key={item.name} item={item} />
+            ))}
+          </ul>
+          <button 
+            onClick={() => navigate("/register")}
+            className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300"
+          >
+              Sign in
+          </button>
+        </div>
       </header>
     </section>
   );
 };
-
 export default Header;
