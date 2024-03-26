473e9f7774ede91ea56f71eab93903cc27a4889e
sthaanshuu <anshushrestha9@gmail.com>
2024-03-26 21:07:14 +0545

    Comment Management/Admin panel

--- src/services/index/comments.js
+++ src/services/index/comments.js
@@ -35,6 +35,7 @@ export const createNewComment = async ({
 export const updateComment = async ({
     token,
     desc,
+    check,
     commentId
 }) => {
   try {
@@ -48,6 +49,7 @@ export const updateComment = async ({
         `/api/comments/${commentId}`, 
         {
             desc,
+            check,
         }, 
         config
     );
@@ -72,6 +74,31 @@ export const deleteComment = async ({
 
     const { data } = await axios.delete(`/api/comments/${commentId}`, config);
     return data;
+  } catch (error) {
+    if (error.response && error.response.data.message)
+      throw new Error(error.response.data.message);
+    throw new Error(error.message);
+  }
+};
+
+export const getAllComments = async (
+  token,
+  searchKeyword = "",
+  page = 1,
+  limit = 10
+) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const { data, headers } = await axios.get(
+      `/api/comments?searchKeyword=${searchKeyword}&page=${page}&limit=${limit}`,
+      config
+    );
+    return { data, headers };
   } catch (error) {
     if (error.response && error.response.data.message)
       throw new Error(error.response.data.message);
